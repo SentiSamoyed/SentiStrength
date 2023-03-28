87c3336dc120f3b1e2dcfa8f656a7ec08466508e
tanziyue <201250093@smail.nju.edu.cn>
2023-03-28 10:52:27 +0800

    feat: 引入 Spring 框架；引入测试；

---
+++ src/test/java/sample/SampleTest.java
@@ -0,0 +1,18 @@
+package sample;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author tanziyue
+ * @date 2023/3/28
+ * @description 一个测试样例类，展示了测试支持的基本功能
+ */
+@Log4j2
+public class SampleTest {
+  @Test
+  void sampleTest() {
+    log.info("dwqdq");
+  }
+}
