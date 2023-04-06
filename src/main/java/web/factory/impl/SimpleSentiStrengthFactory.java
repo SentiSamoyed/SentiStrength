6ef8f9e2b125134b417d875f18061ca51480b979
tanziyue <201250093@smail.nju.edu.cn>
2023-04-06 11:05:05 +0800

    feat: web 中 SentiStrength 工厂类

---
+++ src/main/java/web/factory/impl/SimpleSentiStrengthFactory.java
@@ -0,0 +1,81 @@
+package web.factory.impl;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import uk.ac.wlv.sentistrength.SentiStrength;
+import web.data.vo.AnalysisOptionsVO;
+import web.enums.AnalysisModeEnum;
+import web.factory.SentiStrengthFactory;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author tanziyue
+ * @date 2023/4/6
+ * @description 最简单的 SentiStrength 构建策略
+ */
+@Component
+@Log4j2
+public class SimpleSentiStrengthFactory implements SentiStrengthFactory {
+
+  private final String sentiDataPath;
+
+  public SimpleSentiStrengthFactory() throws IOException {
+    sentiDataPath = new ClassPathResource("SentStrength_Data").getFile().getAbsolutePath();
+  }
+
+  @Override
+  public SentiStrength build(AnalysisModeEnum mode, Boolean explain, AnalysisOptionsVO options) {
+    SentiStrength sentiStrength = new SentiStrength();
+    List<String> args = getArgs(mode, explain, options);
+
+    args.add("sentidata");
+    args.add(sentiDataPath);
+
+    sentiStrength.initialise(args.toArray(new String[0]));
+    return sentiStrength;
+  }
+
+  private List<String> getArgs(AnalysisModeEnum mode, Boolean explain, AnalysisOptionsVO options) {
+    List<String> args = convertOptions(options);
+    if (!mode.equals(AnalysisModeEnum.DEFAULT)) {
+      args.add(mode.getVal());
+    }
+
+    if (explain) {
+      args.add("explain");
+    }
+
+    return args;
+  }
+
+  private List<String> convertOptions(AnalysisOptionsVO optionsVO) {
+    List<String> args = new LinkedList<>();
+
+    Method[] methods = optionsVO.getClass().getMethods();
+    for (Method method : methods) {
+      // For each getter
+      String name = method.getName();
+      if (name.startsWith("get") && !"getClass".equals(name)) {
+        try {
+          Object val = method.invoke(optionsVO);
+          String key = name.substring("get".length());
+          args.add(key);
+
+          if (!(val instanceof Boolean)) {
+            args.add(val.toString());
+          }
+        } catch (Exception e) {
+          log.fatal(e.getLocalizedMessage());
+          throw new RuntimeException(e);
+        }
+      }
+    }
+
+    return args;
+  }
+}
