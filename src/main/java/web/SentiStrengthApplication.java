b34f61a928f876b6a462cd2abca47f58ca15f0c7
tanziyue <201250093@smail.nju.edu.cn>
2023-04-06 16:56:40 +0800

    fix: 构建脚本

--- src/main/java/web/SentiStrengthApplication.java
+++ src/main/java/web/SentiStrengthApplication.java
@@ -13,14 +13,25 @@ import java.io.File;
 public class SentiStrengthApplication {
   public static void main(String[] args) {
     if (args.length != 0 && "--web".equals(args[0])) {
-      Assert.isTrue(args.length >= 2, "Usage: ~ --web <Path to SentiStrength_Data>");
+      if (args.length < 2) {
+        usageAndExit();
+      }
+      
       File f = new File(args[1]);
-      Assert.isTrue(f.exists() && f.isDirectory(), "Invalid path of SentiStrength_Data: " + args[1]);
-      SentiData.SENTI_DATA_DIR_PATH = args[1];
+      if (!f.exists() || !f.isDirectory()) {
+        System.err.println("Invalid path of SentiStrength_Data: " + args[1]);
+        usageAndExit();
+      }
 
+      SentiData.SENTI_DATA_DIR_PATH = args[1];
       SpringApplication.run(SentiStrengthApplication.class, args);
     } else {
       SentiStrength.main(args);
     }
+  }
+
+  private static void usageAndExit() {
+    System.err.println("Usage: ~ --web <Path to SentiStrength_Data>");
+    System.exit(1);
   }
 }
