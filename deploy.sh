5c5b483d314a3ae473b2cef7f970a1113ddecda3
tanziyue <201250093@smail.nju.edu.cn>
2023-04-07 09:25:38 +0800

    feat: 更新构建方式为 docker compose

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-C_NAME=sentistrength
-I_NAME=sentistrength:latest
+docker compose down;
 
-# 删除已存在的 Docker container
-if docker container ls | grep -q "$C_NAME"; then
-  docker container stop "$C_NAME"; # 启动时 --rm，无需手动删除
-fi;
-
-# 建立新的 container
-docker run --rm -d -p "8848:8080" --name "$C_NAME" "$I_NAME"
+docker compose up -d;
