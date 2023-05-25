36e38938ee56290ea0c0e045c47ab5b883e7e327
tanziyue <201250093@smail.nju.edu.cn>
2023-05-25 10:18:49 +0800

    wip: tendency

--- src/main/java/web/dao/IssueRepository.java
+++ src/main/java/web/dao/IssueRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import web.data.dto.TendencySummarizedDataDTO;
 import web.entity.po.IssuePO;
+
+import java.util.List;
 
 @Repository
 public interface IssueRepository extends PagingAndSortingRepository<IssuePO, Long>, CrudRepository<IssuePO, Long> {
@@ -18,4 +21,48 @@ public interface IssueRepository extends PagingAndSortingRepository<IssuePO, Lon
        """)
   void updateSentimentScores(@Param("id") long id, @Param("posVal") int posVal,
                              @Param("negVal") int negVal, @Param("scaleVal") int scaleVal);
+
+
+
+  @Query(value = """
+          SELECT `t`.`year`                                                     AS `milestone`,
+                 SUM(`t`.`sum`) OVER (ORDER BY `year` ROWS UNBOUNDED PRECEDING) AS `sum`,
+                 SUM(`count`) OVER (ORDER BY `year` ROWS UNBOUNDED PRECEDING)   AS `count`
+          FROM (SELECT YEAR(`created_at`) AS `year`, SUM(`scale_val`) AS `sum`, COUNT(`scale_val`) AS `count`
+                FROM `issue`
+                WHERE `repo_full_name` = :fullName
+                GROUP BY `year`
+                ORDER BY `year`) AS `t`;
+      """, nativeQuery = true)
+  List<TendencySummarizedDataDTO> getTendencyDataByYear(@Param("fullName") String fullName);
+
+  @Query(value = """
+          SELECT CONCAT(`t`.`year`, '.', LPAD(`t`.`month`, 2, '0'))             AS `milestone`,
+                 SUM(`t`.`sum`) OVER (ROWS UNBOUNDED PRECEDING) AS `sum`,
+                 SUM(`count`) OVER (ROWS UNBOUNDED PRECEDING)   AS `count`
+          FROM (SELECT YEAR(`created_at`)  AS `year`,
+                       MONTH(`created_at`) AS `month`,
+                       SUM(`scale_val`)    AS `sum`,
+                       COUNT(`scale_val`)  AS `count`
+                FROM `issue`
+                WHERE `repo_full_name` = :fullName
+                GROUP BY `year`, `month`
+                ORDER BY `year`, `month`) AS `t`;
+      """, nativeQuery = true)
+  List<TendencySummarizedDataDTO> getTendencyDataByYearMonth(@Param("fullName") String fullName);
+
+  @Query(value = """
+          SELECT CONCAT(`t`.`year`, 'Q', `quarter`)                             AS `milestone`,
+                 SUM(`t`.`sum`) OVER (ROWS UNBOUNDED PRECEDING) AS `sum`,
+                 SUM(`count`) OVER (ROWS UNBOUNDED PRECEDING)   AS `count`
+          FROM (SELECT YEAR(`created_at`)    AS `year`,
+                       QUARTER(`created_at`) AS `quarter`,
+                       SUM(`scale_val`)      AS `sum`,
+                       COUNT(`scale_val`)    AS `count`
+                FROM `issue`
+                WHERE `repo_full_name` = :fullName
+                GROUP BY `year`, `quarter`
+                ORDER BY `year`, `quarter`) AS `t`;
+      """, nativeQuery = true)
+  List<TendencySummarizedDataDTO> getTendencyDataByYearQuarter(@Param("fullName") String fullName);
 }
---
+++ src/main/java/web/data/dto/TendencySummarizedDataDTO.java
@@ -0,0 +1,25 @@
+package web.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TendencySummarizedDataDTO {
+  /**
+   * 里程碑，如：2020 / 2020.Q3 / 2020.03
+   */
+  private String milestone;
+  /**
+   * 截至该里程碑的数值总和
+   */
+  private Integer sum;
+  /**
+   * 截至该里程碑的条目总数
+   */
+  private Integer count;
+}
